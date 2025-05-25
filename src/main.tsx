c2b0d986ca25f0cd4a123713bdf03691ea7ba492
Jinyoung Choi <jinyoung5497@gmail.com>
2025-05-25 20:49:53 +0900

    add: react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+// index.tsx or main.tsx
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
